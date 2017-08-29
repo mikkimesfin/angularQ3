0b130fbd90339d76a8ef0a18df1781d87cef1ecf
Mikki Mesfin <mikki.mesfin@gmail.com>
2017-08-29 10:50:19 -0600

    initial commit

---
+++ form/component.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict'
+  angular.module('angular1.6-inbox', )
+  .component('ngform', {
+    controller: 'formController',
+    templateUrl: 'form/temp.html'
+
+  })
+}())
---
+++ form/controller.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+  angular.module('angular1.6-inbox', )
+  .controller('formController', function(){
+    console.log('hey');
+  })
+}());
